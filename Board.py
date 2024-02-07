bce5a9442c3fb32fefdf8ba32bf34fc930706074
zainchalisa <zain.chalisa@gmail.com>
2024-02-07 15:10:46 -0500

    board file

---
+++ Board.py
@@ -0,0 +1,9 @@
+class Board:
+    def __init__(self):
+        self.rows = 15
+        self.cols = 15
+        self.target = None
+        self.inital = None
+
+    def createBoard (rows, cols, target, inital):
+        pass
