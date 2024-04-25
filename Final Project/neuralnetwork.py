feec29021a65e2ea3026a428553ede76580ec25e
aka1218 <akash121801@gmail.com>
2024-04-25 19:37:37 -0400

    Worked on neural networks

--- Final Project/neuralnetwork.py
+++ Final Project/neuralnetwork.py
@@ -60,7 +60,7 @@ def loadDataFile(filename, n, width, height):
             # We encountered the end of the file
             print("Truncating at %d examples (maximum)" % i)
             break
-        items.append(Perceptron(data, DATUM_WIDTH, DATUM_HEIGHT))
+        items.append(Neural_Network(data, DATUM_WIDTH, DATUM_HEIGHT))
         count = + 1
     print(count)
     return items
@@ -105,16 +105,33 @@ def convertToInteger(data):
     return IntegerConversionFunction(data)
   else:
     return map(convertToInteger, data)
-  
+
+def sigmoid_activation(num):
+
+  return (1/(1 + np.exp(-num)))
+
+def sigmoid_derivative(x):
+    
+    return x * (1 - x)
+
+def backprop(hidden_layer_weights, output_weights, actual, predicted):
+   pass
+
+def softmax(z):
+    e_z = np.exp(z - np.max(z))  
+    return e_z / e_z.sum(axis=0)
+   
 
 def nn_face(n):
   
   epochs = 10
   data = loadDataFile('data/facedata/facedatatrain', 451, 60, 70)
   labels = loadLabelsFile('data/facedata/facedatatrainlabels', 451)
-  weights = np.random.randint(low= -400, high= 400, size=(1000, 70, 60)) # we need different weights for each of the nodes on the hidden layer
-  biases = np.random.randint(low=-200, high=200, size=1000)
-  hidden_layer = [] 
+  hidden_weights = np.random.randint(low= -400, high= 400, size=(1000, 70, 60)) # we need different weights for each of the nodes on the hidden layer
+  output_weights = np.random.randint(low= -400, high= 400, size=1000)
+  hidden_biases = np.random.randint(low=-200, high=200, size=1000)
+  output_bias = np.random.randint(low = -200, high = 200, size = 1)
+  hidden_layer_values = [] 
   num_samples = int(n * 451)
 
     # when creating the neural network we need the following:
@@ -123,25 +140,74 @@ def nn_face(n):
         # the output layer (this layer is computed using sigmoid of each of the values computed in the hidden layer)
 
   for epoch in range(epochs):
+
     for _ in num_samples:
       
       idx = np.random.randint(0, 451)
       sample = data[idx]
       
-      
+      for node in range(1000):
+        total_sum = 0
+        for i in range(70):
+          for j in range(60):
+              total_sum += hidden_weights[node][i][j] * sample.getPixel(i, j)
+
+        hidden_layer_values.append(sigmoid_activation(total_sum + hidden_biases[node]))    
         
 
+      final_output = 0
+    
+      for i in range(len(hidden_layer_values)):
+          final_output += output_weights[i] * hidden_layer_values[i]
+
+      face_probability = sigmoid_activation(final_output + output_bias)
+
+      if face_probability > 0.5 and labels[idx] == 0:
+          pass
+          #backprop(output_weights, hidden_layer_)
+      elif face_probability < 0.5 and labels[idx] == 1:
+          pass
+          #backprop(weights)
 
 
 
 def nn_digit(n):
   
   epochs = 10
-  data = loadDataFile('data/digitdata/trainingimages', 5000, 28, 28)
-  labels = loadLabelsFile('data/digitdata/traininglabels', 5000)
-  weights = np.random.randint(low=-400, high=400, size=(10, 28, 28))
-  num_samples = int(n * 5000)
+  data = loadDataFile('data/digitdata/trainingimages', 451, 28, 28)
+  labels = loadLabelsFile('data/digitdata/traininglabels', 451)
+  hidden_weights = np.random.randint(low= -400, high= 400, size=(256, 28, 28)) # we need different weights for each of the nodes on the hidden layer
+  output_weights = np.random.randint(low= -400, high= 400, size=(10, 256))
+  hidden_biases = np.random.randint(low=-200, high=200, size=256)
+  output_biases = np.random.randint(low = -200, high = 200, size = 10)
+  hidden_layer_values = [] 
+  num_samples = int(n * 451)
 
+    # when creating the neural network we need the following:
+        # the input layer (this layer will be the individual pixels of the image)
+        # the hidden layer (this layer will use the sigmoid function g(Z) = (1/(1 + e^-2)))
+        # the output layer (this layer is computed using sigmoid of each of the values computed in the hidden layer)
+
+  for epoch in range(epochs):
+
+    for _ in num_samples:
+      
+      idx = np.random.randint(0, 451)
+      sample = data[idx]
+      
+      for node in range(256):
+        total_sum = 0
+        for i in range(28):
+          for j in range(28):
+              total_sum += hidden_weights[node][i][j] * sample.getPixel(i, j)
+
+        hidden_layer_values.append(sigmoid_activation(total_sum + hidden_biases[node]))    
+        
+
+      final_output = np.dot(output_weights, hidden_layer_values) + output_biases
+      digit_predicted = np.argmax(final_output)
+      
+      
 
 
 def _test():
